4f9de00cd806f52fd439e4b18623938c3f3b8a99
Francisco Bezerra da Silva Junior <franciscojunior02@hotmail.com.br>
2024-01-10 17:02:10 -0300

    theme config

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyles } from './styles/global'
+
 function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
       <p>Hello World</p>
-    </>
+      <h1>Hello World</h1>
+    </ThemeProvider>
   )
 }
 
